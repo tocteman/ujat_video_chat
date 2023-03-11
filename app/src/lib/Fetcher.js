6a8aabdd76159280effd472984b204800550acdb
tocteman <juanfrancisco.vinueza@gmail.com>
2023-03-11 17:03:05 -0500

    api url env

--- app/src/lib/Fetcher.js
+++ app/src/lib/Fetcher.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = "http://127.0.0.1:8000"
+const apiBaseUrl = import.meta.env.VITE_API_URL
 
 export const apiPost = async (suffix, body) => {
   const url = `${apiBaseUrl}/${suffix}`
